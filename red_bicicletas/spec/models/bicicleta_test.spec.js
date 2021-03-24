b4479fe6b27ecd2caadfdc90f46da82ca7d539b8
latinogan <roger.castillo.arce@gmail.com>
2021-03-24 17:43:50 -0500

    se hace interacion en el testing y se cambia el puerto a 5000

--- red_bicicletas/spec/models/bicicleta_test.spec.js
+++ red_bicicletas/spec/models/bicicleta_test.spec.js
@@ -1,14 +1,14 @@
 var Bicicleta = require("../../models/bicicleta");
 
-describe('Bicicleta.allBicis'.() => {
-	it('comienza vacia' . () => {
+describe('Bicicleta.allBicis',() => {
+	it('comienza vacia' , () => {
 		expect(Bicicleta.allBicis.length).toBe(0);
 
 	});
 });
 
-describe('Bicicleta.add'. () => {
-	it('agregamos una'. () => {
+describe('Bicicleta.add', () => {
+	it('agregamos una', () => {
 		expect(Bicicleta.allBicis.length).toBe(0);
 
 		var a = new Bicicleta (1, 'rojo', 'urbana', [-32.6012424,-58.3861497]);
