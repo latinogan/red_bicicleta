e1e696df63e6ce252b271810ac3293e4a6f22a48
latinogan <roger.castillo.arce@gmail.com>
2021-03-28 21:08:47 -0500

    se agrega seguridad al modelo se usuario

---
+++ red_bicicletas/spec/models/usuario_test.spec.js
@@ -0,0 +1,47 @@
+var mongoose = require ('mongoose');
+var Bicicleta = require ('../../models/bicicleta');
+var Usuario = require ('../../models/usuario');
+var Reserva = require ('../../models/reserva');
+
+describe ('Testing Usuarios', function(){
+	beforeEach ( function (done) {
+		var mongoDB = 'mongodb://localhost/testdb';
+		mongoose.connect(mongoDB, {userNewUrlParser: true});
+
+		const db = mongoose.connection;
+		db.on('error', console.error.bind (console. 'connection error'));
+		db.once ('open', function () {
+			console.log('We are conected to test data base');
+
+			done();
+		});
+	});
+
+	afterEach ( function (done) {
+		Reserva.deleteMany ( {}, function ( err,success){
+			it ( err) console.log(err);
+			Usuario.deleteMany({}, function (err,success) {
+				it (err) console.log (err);
+				Bicicleta.deleteMany({}, function (err, success){
+					it (err) console.log (err);
+					done();
+				});
+
+			});
+		});
+	});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
